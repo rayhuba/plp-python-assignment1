24ca4c6ac93ce11f6592d47b6c48773d3703533c
rayhuba <huba855@gmail.com>
2024-03-06 18:23:53 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# The first assignment in python on the plp program
+#user_input.py
+
+#Ask for the user's name.He/she should input the name
+name = input("kindly enter your name: ")
+
+#Asks for the user's age 
+age = input("Kindly enter your age: ")
+
+#Asks the user for he/her location
+location = input("Kindly enter the geographical location that you live in:")
+
+#Printing the personalized message
+print("Hello " + name + ",you are " + age + " years old and you live in " + location +".")
